162906f1b8a656972eb82156b8848cd78ebba235
Ezequiel <cruz.ezequiel.p@gmail.com>
2023-05-19 20:54:18 -0300

    Se hicieron pruebas con inputs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
 
+  inputUser: string = '';
+
+  inputPassword: string = '';
+
+  test() {
+    console.log(this.inputUser);
+    console.log(this.inputPassword);
+  }
 }
