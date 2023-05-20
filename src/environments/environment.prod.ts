632eb75bad7131f29652fda3bb6729d885cfd377
Ezequiel <cruz.ezequiel.p@gmail.com>
2023-05-20 14:36:54 -0300

    Se modifico enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDQLkgCsYeS34hcE8dlDrzVHVpBH9F00Ps',
+    authDomain: 'clothes-login-e1d62.firebaseapp.com',
+    projectId: 'clothes-login-e1d62',
+    storageBucket: 'clothes-login-e1d62.appspot.com',
+    messagingSenderId: '781100837730',
+    appId: '1:781100837730:web:1934ed6ae27aeaacd51a3b',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDQLkgCsYeS34hcE8dlDrzVHVpBH9F00Ps',
+    authDomain: 'clothes-login-e1d62.firebaseapp.com',
+    projectId: 'clothes-login-e1d62',
+    storageBucket: 'clothes-login-e1d62.appspot.com',
+    messagingSenderId: '781100837730',
+    appId: '1:781100837730:web:1934ed6ae27aeaacd51a3b',
+  },
 };
 
 /*
