59832459adb541598a2bc6f24c9f33f52d0eb5ed
Ezequiel <cruz.ezequiel.p@gmail.com>
2023-06-02 00:57:45 -0300

    Se realizaron cambios en las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./components/login/login.module').then((m) => m.LoginPageModule),
+  },
+
+  {
+    path: 'registro',
+    loadChildren: () =>
+      import('./components/registro/registro.module').then(
+        (m) => m.RegistroPageModule
+      ),
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/components/login/login.page.ts
+++ src/app/components/login/login.page.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
 })
-export class Tab1Page {
+export class LoginPage implements OnInit {
   constructor(public autho: Auth) {}
+
+  ngOnInit() {}
 
   inputUser: string = '';
 
