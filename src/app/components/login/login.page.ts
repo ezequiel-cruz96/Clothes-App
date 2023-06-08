4b965aaf10c3fd6ec2db6b06d1fe19576a008744
Ezequiel <cruz.ezequiel.p@gmail.com>
2023-06-08 01:22:03 -0300

    Se integro logica a los formularios

--- src/app/components/login/login.page.ts
+++ src/app/components/login/login.page.ts
@@ -29,7 +29,7 @@ export class LoginPage implements OnInit {
   login() {
     signInWithEmailAndPassword(this.autho, this.inputUser, this.inputPassword)
       .then(() => {
-        this.errors = 'Login exitoso';
+        this.router.navigate(['/menu']);
       })
       .catch((err) => {
         let mailError :string ="Firebase: Error (auth/invalid-email)."
--- src/app/components/registro/registro.page.ts
+++ src/app/components/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
+import { AlertController } from '@ionic/angular';
 
 import { Router } from '@angular/router';
 
@@ -8,19 +10,52 @@ import { Router } from '@angular/router';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
-  constructor(public router: Router) {}
+  constructor(
+    public router: Router,
+    private auth: Auth,
+    private alertController: AlertController
+    ) {}
 
   ngOnInit() {}
 
-  toMenu() {
-    this.router.navigate(['/menu']);
+  registerUser() {
+    createUserWithEmailAndPassword(this.auth, this.emailRegister, this.passwordRegister)
+      .then(() => {
+        this.router.navigate(['/login']);
+      })
+      .catch((err) => {
+        this.presentAlert(err)
+      });
   }
-  menuType: string = 'overlay';
 
+    async presentAlert(err :string) {
+
+    let errorFirebase="Firebase: Password should be at least 6 characters (auth/weak-password)"
+
+    let errors = err != errorFirebase ? "El correo ya esta registrado" : ""
+
+    const alert = await this.alertController.create({
+      message: errors,   
+    });
+    await alert.present();
+  }
+
+   validate(){
+    if(this.emailRegister){
+       this.validateEmail(this.emailRegister) ? this.validateField = false :this.validateField = true
+    }
+  }
+
+   validateEmail (inputUser: any)  {
+    return inputUser.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  }
   
-  inputUser: string = '';
+  validateField=false
 
-  inputPassword: string = '';
+  emailRegister: string = '';
 
-  errors: string = '';
+  passwordRegister: string = '';
+
 }
