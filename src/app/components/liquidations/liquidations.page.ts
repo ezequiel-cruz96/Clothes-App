c2d9865e5e5327b4adcce11d3dec10a311ffc6aa
Ezequiel <cruz.ezequiel.p@gmail.com>
2023-06-19 20:58:16 -0300

    Se refactorizo la vista de liquidaciones

--- src/app/components/liquidations/liquidations.page.ts
+++ src/app/components/liquidations/liquidations.page.ts
@@ -4,10 +4,6 @@ import { Location } from "@angular/common";
 import { Router } from '@angular/router';
 import { HttpProvider } from 'src/app/provider/http.provider';
 
-import { Firestore, collection, addDoc, collectionData, doc, getDoc,getFirestore ,deleteDoc} from '@angular/fire/firestore'
-
-
-
 @Component({
   selector: 'app-liquidations',
   templateUrl: './liquidations.page.html',
@@ -19,57 +15,25 @@ export class LiquidationsPage implements OnInit {
     public router:Router,
     public service: HttpProvider, 
     private location : Location,
-    private firestore :Firestore
   ) {}
 
-  async ngOnInit() {
-    await this.getStock()
-    await this.getDolars()
+  ngOnInit() {
+    this.getDolars()
   }
 
-  clothesPrice: any 
-  
   dolarBlue: any
 
   dolars: any 
 
-  actualPrice: any 
-
-  actualPriceRound: any 
-
-    getDolars(){
-      ( 
-      this.service.getTypesDolar()).subscribe(
-      (data: any) => { this.updateData(data)},
-      (error: any) => { console.log(error);}
-      )
+  getDolars(){( 
+    this.service.getTypesDolar()).subscribe(
+    (data: any) => {  this.dolars = data},
+    (error: any) => { console.log(error);}
+    )
   }
 
-  updateData(data: any){
-    this.dolars= data
-    this.dolarBlue = this.dolars[1].casa.venta
-    this.actualPrice= this.clothesPrice.remera.precio / parseFloat(this.dolarBlue)
-    this.actualPriceRound = Math.round(this.actualPrice)
-  }
-
-
-  async getStock(){
-    const db = getFirestore();
-
-    const docRef = doc(db, "Stock", "stock");
-
-    const docSnap = await getDoc(docRef);
-
-    docSnap.data();
-
-    try {
-      const docSnap = await getDoc(docRef);
-
-      this.clothesPrice = docSnap.data()
-    } 
-    catch(error) {
-      console.log(error)
-    }
+  toProducts(){
+    this.router.navigate(['/products'])
   }
 
   toMenu(){
@@ -79,6 +43,5 @@ export class LiquidationsPage implements OnInit {
   toBack(){
     this.location.back();
   }
-
 
 }
